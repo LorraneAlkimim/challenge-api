c9fde1923ff556f870a29c4f391f734a5540f3ed
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-02 23:27:22 -0300

    feat: creates Sale model and adds to admin
    Creates Sale model and an intermediary table SaleProduct to handle items of a sale and his methods

--- amcom_project/core/admin.py
+++ amcom_project/core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Customer, Seller, Product
+from .models import Customer, Seller, Product, Sale, SaleProduct
+
+class SaleProductInline(admin.TabularInline):
+  model = SaleProduct
+  extra = 1
+
+class SaleAdmin(admin.ModelAdmin):
+  inlines = (SaleProductInline,)
 
 admin.site.register(Customer)
 admin.site.register(Seller)
 admin.site.register(Product)
+admin.site.register(Sale, SaleAdmin)
--- amcom_project/core/models.py
+++ amcom_project/core/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from datetime import datetime
+from django.db.models import DateTimeField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -32,3 +34,43 @@ class Product(models.Model):
 
   def __str__(self):
     return self.description
+
+
+class Sale(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  invoice_code = models.CharField(max_length = 50)
+  date = models.DateTimeField(default=datetime.now, blank=True)
+  customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
+  seller = models.ForeignKey(Seller, on_delete=models.RESTRICT)
+  products = models.ManyToManyField(Product, through='SaleProduct')
+
+  def calculate_sale_total_value(self):
+    total_value = sum(product.calculate_total_price() for product in self.saleproduct_set.all())
+    return total_value
+  
+  def calculate_total_commission(self):
+    total_commission = sum(product.calculate_product_commission() for product in self.saleproduct_set.all())
+    return total_commission
+  
+  def count_items(self):
+    quantity_of_items = sum(product.quantity for product in self.saleproduct_set.all())
+    return quantity_of_items
+
+
+class SaleProduct(models.Model):
+  product = models.ForeignKey('Product', on_delete=models.CASCADE)
+  sale = models.ForeignKey('Sale', on_delete=models.CASCADE)
+  quantity = models.IntegerField(default=1)
+
+  def calculate_total_price(self):
+    return self.product.price * self.quantity
+  
+  def calculate_product_commission(self):
+    product_total_price = self.calculate_total_price()
+    product_commission = (self.product.commission_percentage/100) * product_total_price
+    return round(product_commission, 2)
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=('product', 'sale'), name='once_per_sale_product')
+    ]
