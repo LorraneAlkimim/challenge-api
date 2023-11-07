e83774bd8d2d85960ef470d50fe6d68204c2add7
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-07 01:43:19 -0300

    refactor: removes Sale from admin
    Remove Sale from the Django admin, its management is only via API.

--- amcom_project/core/admin.py
+++ amcom_project/core/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Seller, Product, Sale, SaleProduct, CommissionPercentageByWeekday
-
-class SaleProductInline(admin.TabularInline):
-  model = SaleProduct
-  extra = 1
-
-class SaleAdmin(admin.ModelAdmin):
-  inlines = (SaleProductInline,)
+from .models import Customer, Seller, Product, CommissionPercentageByWeekday
 
 admin.site.register(Customer)
 admin.site.register(Seller)
 admin.site.register(Product)
-admin.site.register(Sale, SaleAdmin)
 admin.site.register(CommissionPercentageByWeekday)
