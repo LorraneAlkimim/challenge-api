815ccc1051b8bd8beb9eecaf14eebb494f1d2a47
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-07 01:39:43 -0300

    feat: adds serializer, viewset and url for Sale model

--- amcom_project/core/views.py
+++ amcom_project/core/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
-from .serializers import CustomerSerializer, SellerSerializer, ProductSerializer
-from .models import Customer, Seller, Product
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CustomerSerializer, SellerSerializer, ProductSerializer, SaleSerializer, SaleCreateSerializer, SaleUpdateSerializer
+from .models import Customer, Seller, Product, Sale
 
 
 class CustomerViewset(viewsets.ModelViewSet):
@@ -14,3 +17,58 @@ class SellerViewset(viewsets.ModelViewSet):
 class ProductViewset(viewsets.ModelViewSet):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+
+class SaleViewset(viewsets.ModelViewSet):
+  queryset = Sale.objects.all()
+  serializer_class = SaleSerializer
+  http_method_names = ['get', 'delete']
+
+class SaleCreateViewset(APIView):
+  def post(self, request):
+    serializer = SaleCreateSerializer(data=request.data)
+    
+    if serializer.is_valid():
+      sale = serializer.save()
+
+      return Response({'id': sale.invoice_code}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SaleUpdateViewset(APIView):
+  def put(self, request, invoice_code):
+    try:
+      sale = Sale.objects.get(invoice_code=invoice_code)
+    except Sale.DoesNotExist:
+      return Response({"detail": "Sale not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = SaleUpdateSerializer(sale, data=request.data)
+    if serializer.is_valid():
+      products = request.data.get('products', None)
+      if products is not None:
+        sale.products.clear()
+
+        for product_data in products:
+          product_id = product_data['id']
+          quantity = product_data['quantity']
+          sale.products.add(product_id, through_defaults={'quantity': quantity})
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  def patch(self, request, invoice_code):
+    try:
+        sale = Sale.objects.get(invoice_code=invoice_code)
+    except Sale.DoesNotExist:
+      return Response({"detail": "Sale not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = SaleUpdateSerializer(sale, data=request.data, partial=True)
+    if serializer.is_valid():
+      products = request.data.get('products', None)
+      if products is not None:
+          for product_data in products:
+              product_id = product_data['id']
+              quantity = product_data['quantity']
+              
+              sale.products.update_or_create(product_id, defaults={'quantity': quantity})
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- amcom_project/urls.py
+++ amcom_project/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from amcom_project.core.views import CustomerViewset, SellerViewset, ProductViewset
+from amcom_project.core.views import CustomerViewset, SellerViewset, ProductViewset, SaleViewset, SaleCreateViewset, SaleUpdateViewset
 
 
 router = routers.DefaultRouter()
 router.register(r'customers', CustomerViewset)
 router.register(r'sellers', SellerViewset)
 router.register(r'products', ProductViewset)
+router.register(r'sales', SaleViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/create_sale/', SaleCreateViewset.as_view()),
+    path('api/update_sale/<int:invoice_code>/', SaleUpdateViewset.as_view())
 ]
