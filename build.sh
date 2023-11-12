91dc7090efdb3b6ece1559e6bb77a52d26f4ada1
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2023-11-11 21:13:40 -0300

    fix: fixes dependencies install command

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
